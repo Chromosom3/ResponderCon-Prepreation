b3a22c2b4dcc6d28608cb1a347cb23f5e46ac785
Chromosom3 <36739730+Chromosom3@users.noreply.github.com>
2022-09-02 20:00:55 -0400

    Made a script to make user accounts.

---
+++ windows-automation/user_creation.ps1
@@ -0,0 +1,16 @@
+# Script: user_creation.ps1
+# Author: Dylan 'Chromosom3' Navarro
+# Description: Used to make user accounts in bulk
+
+# Configuration
+$OU_Location = "OU=Accounts,OU=Workshop,DC=Domain,DC=local"
+$defaultPass = "Password123!@#"
+$number_of_users = 33
+
+# Don't edit below this line
+$number_of_users += 1
+for (0 -lt $number_of_users; $number_of_users = $number_of_users - 1){
+    $username = "user" + $number_of_users
+    Write-Host("Number of Users: $username")
+    New-ADUser -SamAccountName $username -Name $username -Path $OU_Location -AccountPassword (ConvertTo-SecureString -AsPlainText $defaultPass -Force)
+}
